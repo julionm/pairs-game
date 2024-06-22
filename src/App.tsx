14c7c016ebc9f1c10b963f4673bbaa3dd988917e
Julio Negri <julioyuki@outlook.com>
2024-06-22 13:52:05 -0300

    fix: Fixing tailwind installation

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <div>
+    <div id="board" className='bg-red h-screen w-full'>
       
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./index.html",
+    "./src/**/*.{tsx}"
+  ],
   theme: {
     extend: {},
   },
