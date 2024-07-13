4b87aafe9a59b909ddc4155d8c77ba19f7cbc288
Julio Negri <julioyuki@outlook.com>
2024-07-13 16:07:52 -0300

    fix: Fixing wrong import in PairsGame component

--- src/pages/PairsGame/index.tsx
+++ src/pages/PairsGame/index.tsx
@@ -1,46 +1,46 @@
 import { PairsGame } from "components/PairsGame";
-import { Card, CardValueType } from "models/cards";
+import { Card, Types } from "models/cards";
 
 const PAIRS: Card[][] = [
     [
-      { id: 1, value: "か", type: CardValueType.JAPANESE },
-      { id: 2, value: "KA", type: CardValueType.STRING }
+      { id: 1, value: "か", type: Types.JAPANESE },
+      { id: 2, value: "KA", type: Types.STRING }
     ],
     [
-      { id: 3, value: "あ", type: CardValueType.JAPANESE },
-      { id: 4, value: "A", type: CardValueType.STRING }
+      { id: 3, value: "あ", type: Types.JAPANESE },
+      { id: 4, value: "A", type: Types.STRING }
     ],
     [
-      { id: 5, value: "い", type: CardValueType.JAPANESE },
-      { id: 6, value: "I", type: CardValueType.STRING }
+      { id: 5, value: "い", type: Types.JAPANESE },
+      { id: 6, value: "I", type: Types.STRING }
     ],
     [
-      { id: 7, value: "う", type: CardValueType.JAPANESE },
-      { id: 8, value: "U", type: CardValueType.STRING }
+      { id: 7, value: "う", type: Types.JAPANESE },
+      { id: 8, value: "U", type: Types.STRING }
     ],
     [
-      { id: 9, value: "え", type: CardValueType.JAPANESE },
-      { id: 10, value: "E", type: CardValueType.STRING },
+      { id: 9, value: "え", type: Types.JAPANESE },
+      { id: 10, value: "E", type: Types.STRING },
     ],
     [
-      { id: 11, value: "お", type: CardValueType.JAPANESE },
-      { id: 12, value: "O", type: CardValueType.STRING },
+      { id: 11, value: "お", type: Types.JAPANESE },
+      { id: 12, value: "O", type: Types.STRING },
     ],
     [
-      { id: 13, value: "き", type: CardValueType.JAPANESE },
-      { id: 14, value: "KI", type: CardValueType.STRING },
+      { id: 13, value: "き", type: Types.JAPANESE },
+      { id: 14, value: "KI", type: Types.STRING },
     ],
     [
-      { id: 15, value: "く", type: CardValueType.JAPANESE },
-      { id: 16, value: "KU", type: CardValueType.STRING },
+      { id: 15, value: "く", type: Types.JAPANESE },
+      { id: 16, value: "KU", type: Types.STRING },
     ],
     [
-      { id: 17, value: "け", type: CardValueType.JAPANESE },
-      { id: 18, value: "KE", type: CardValueType.STRING }
+      { id: 17, value: "け", type: Types.JAPANESE },
+      { id: 18, value: "KE", type: Types.STRING }
     ],
     [
-      { id: 19, value: "こ", type: CardValueType.JAPANESE },
-      { id: 20, value: "KO", type: CardValueType.STRING }
+      { id: 19, value: "こ", type: Types.JAPANESE },
+      { id: 20, value: "KO", type: Types.STRING }
     ]
   ];
 
